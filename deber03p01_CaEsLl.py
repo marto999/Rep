c9c342fb4d06f959d93d1e6f88dca98d058ba3eb
Oscar Catota <ocatota@gmail.com>
2020-05-03 11:13:42 -0500

    deber 01 y 03

--- deber03p01_CaEsLl.py
+++ deber03p01_CaEsLl.py
@@ -2,32 +2,31 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import random
+def walk3d():
+    walk = [[0,0,0]]
+    step = [0, 0, 0]
+    x = 0
+    for i in range(40):
+        #axis refieres a ejes 0=x 1=y 2=z 
+        axis = random.randrange(0, 3)
+        # aumento o disminucion de paso en ejes
+        step[axis] += random.choice([-1, 1])
+        walk.append(step[:])
+    
+    
+    print (walk)
 
-walk = [[0,0,0]]
-step = [0, 0, 0]
-x = 0
-for i in range(40):
-    #axis refieres a ejes 0=x 1=y 2=z 
-    axis = random.randrange(0, 3)
-    # aumento o disminucion de paso en ejes
-    step[axis] += random.choice([-1, 1])
-    walk.append(step[:])
+def walk4d():
+    walk1= [[0,0,0,0]]
+    step2=
     
 
-boolArr = ( walk == [[0,0,0]])
-result = np.where(boolArr)
-
-print (result)
-
-
-print (walk)
+walk3d()
+#boolArr = ( walk == [[0,0,0]])
+#result = np.where(boolArr)
 
 
-x, y, z = zip(*walk)
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.plot(x, y, z)
-ax.scatter(x[-1], y[-1], z[-1], c='b', marker='o') 
-ax.legend()
-plt.show()
+
+
+
 
