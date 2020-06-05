f9b1ae07a9259be2cfba017134ac14b385b8539f
Martin <martin.escobar@udla.edu.ec>
2020-06-05 12:15:53 -0500

    Deber 3 Literal 1 inverse sampling completo,falta rejection method y literal 2

--- deber03p02_CaEsLl.py
+++ deber03p02_CaEsLl.py
@@ -4,62 +4,45 @@ import matplotlib.pyplot as plt
 
   
 #1.Develop a random-variate generator for a random variable X with the pdf 
-
 x1=np.linspace(-5,0,100)
 fx1=np.e**(2*x1)
 
-x2=np.linspace(5,0,100)
+x2=np.linspace(0,5,100)
 fx2=np.e**(-2*x2)
  
 print(np.trapz(fx1,x1))
 print(np.trapz(fx2,x2))
 print("\n")
 
+############################################################################
+#                      Inverse Transform Sampling                          #
+############################################################################
 
-plt.show()
-
-
-
-#Inverse Transform Sampling (ITS) 
-
+size = 100000
+U = np.random.rand(size)  # Uniform distribution sampling
+AU=[]
 #Inversa f1:  ln(2x)/2
 #Inversa f2:  -ln(-2x)/2
+#U < 0.5  # U distr. uniforme U(0,1)
+#I1(U) else I2(U)  # I inversa de la acumulada
 
+for i in range(len(U)):
+    u=U[i]
+    if(u<0.5):
 
-#Generador de números randómicos
-size=1000
-AX=[]
-AY=[]
+        I1=(np.log(2*u))/2
+        AU.append(I1)    
+    else:
+        I2=(-np.log((-2*u)+2))/2
+        AU.append(I2)    
 
-for i in range(size):
-    randomX=random.uniform(-5, 5) 
-    
+print("AU",AU)
 
-    if(randomX>0 and randomX<0.5):
-        y=(np.log(2*randomX))/2
-        AX.append(randomX)   
-        AY.append(y)
-
-    if(randomX>=0.5 and randomX<=1):
-        nrandomX=randomX*-1
-        y=(-np.log(-2*nrandomX))/2
-        AX.append(randomX)       
-        AY.append(y)
-
-print('\nAX: ',AX)
-print('\nAY: ',AY)
-
-plt.plot(x1,fx1)
-plt.plot(x2,fx2)
-
-#(img.ravel(),256,[0,256])
-
-plt.hist(AY, density=True, bins=100)
+plt.plot(x1,fx1,linewidth=3)
+plt.plot(x2,fx2,linewidth=3)
+plt.hist(AU,density=True,range=[-5,5],facecolor='red', edgecolor='black',bins=100)
 plt.show()
 
-#Rejection method (RM).
-
-
-
-
-
+############################################################################
+#                          Rejection Method                                #
+############################################################################
