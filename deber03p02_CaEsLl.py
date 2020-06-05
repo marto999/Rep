cd2d65062f2e429b18748a27733183921a6c4d21
Martin <martin.escobar@udla.edu.ec>
2020-06-05 15:26:11 -0500

    Deber 3: Inverse Transform Sampling  Completo falta MonteCarlo

--- deber03p02_CaEsLl.py
+++ deber03p02_CaEsLl.py
@@ -1,7 +1,8 @@
 import numpy as np 
-from scipy import random 
-import matplotlib.pyplot as plt
 
+import matplotlib.pyplot as plt
+print('Deber 01: Matching experiment')
+print('Integrantes: Catota-Escobar-LLangarí')
   
 #1.Develop a random-variate generator for a random variable X with the pdf 
 x1=np.linspace(-5,0,100)
@@ -9,14 +10,14 @@ fx1=np.e**(2*x1)
 
 x2=np.linspace(0,5,100)
 fx2=np.e**(-2*x2)
- 
+
 print(np.trapz(fx1,x1))
 print(np.trapz(fx2,x2))
 print("\n")
 
-############################################################################
-#                      Inverse Transform Sampling                          #
-############################################################################
+ ############################################################################
+ #                      Inverse Transform Sampling                          #
+ ############################################################################
 
 size = 100000
 U = np.random.rand(size)  # Uniform distribution sampling
@@ -28,21 +29,81 @@ AU=[]
 
 for i in range(len(U)):
     u=U[i]
+    
     if(u<0.5):
-
-        I1=(np.log(2*u))/2
-        AU.append(I1)    
+         I1=(np.log(2*u))/2
+         AU.append(I1)    
     else:
-        I2=(-np.log((-2*u)+2))/2
-        AU.append(I2)    
+         I2=(-np.log((-2*u)+2))/2
+         AU.append(I2)    
 
 print("AU",AU)
 
 plt.plot(x1,fx1,linewidth=3)
 plt.plot(x2,fx2,linewidth=3)
-plt.hist(AU,density=True,range=[-5,5],facecolor='red', edgecolor='black',bins=100)
+plt.title("Inverse Transform Sampling")
+plt.hist(AU,density=True,facecolor='red', edgecolor='black',bins=100)
 plt.show()
+
+
 
 ############################################################################
 #                          Rejection Method                                #
 ############################################################################
+
+
+
+
+
+
+########################################################################################################
+#                   2.Develop a generation scheme for the triangular distribution with pdf             #                                               #
+########################################################################################################
+
+x1 = np.linspace(2, 3, 100)
+fx1 = 1/2 * (x1-2)
+
+x2 = np.linspace(3, 6, 100)
+fx2 = 1/2 * (2-x2/3)
+
+np.trapz(fx1,x1)
+np.trapz(fx2,x2)
+
+
+
+a=2
+b=6
+c=3
+size = 100000
+U1 = np.random.rand(size)  # Uniform distribution sampling
+
+AU1=[]
+
+#I1= a + np.sqrt((b-a)*(c-a)*u)
+#I2= b - np.sqrt((b-a)(b-c)(1-u))
+
+for i in range(len(U)):
+    u=U1[i]
+    
+    if (u < 0.25):
+         I1=(a + (np.sqrt((b-a)*(c-a)*u)))
+         AU1.append(I1)    
+    else:
+         I2=(b - (np.sqrt((b-a)*(b-c)*(1-u))))
+         AU1.append(I2)    
+
+plt.plot(x1,fx1,linewidth=3)
+plt.plot(x2,fx2,linewidth=3)
+plt.title("Inverse Transform Sampling")
+plt.hist(AU1,density=True,facecolor='red', edgecolor='black',bins=100)
+plt.show()
+
+
+
+
+
+
+
+
+
+
