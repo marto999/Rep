3a417fa0ff16b3d493d04582166d2698da6dd419
AdrianLlg <adrianllangari@gmail.com>
2020-06-06 00:04:10 -0500

    UltimaModDeber02

--- deber02p02_CaEsLl.py
+++ deber02p02_CaEsLl.py
@@ -12,7 +12,6 @@ Un ejemeplar cuesta 50 centavos y se vende a $1.00. Determine el ingreso neto es
 '''
 import numpy as np
 
-<<<<<<< HEAD
 print('------------------------------------------------------')
 print('Deber 02: Generating random variables (discrete)')
 print('Integrantes: LLANGARI-CATOTA-ESCOBAR')
@@ -41,49 +40,11 @@ def SellAll():
     ganancias = sold * 1 - 50*0.50
     #print(a)
     #print(b)
-    print('Probabilidad de que el propietario venda todos los ejemplares en 10000 simulaciones:', severithing)
-    print('Numero promedio esperado de ejemplares no vendidos por dia en 10000 simulaciones:', nosellever)
-    print ('Ingreso promedio neto esperado por dia con 10000 simulaciones: ', ganancias)
+    print('1. Probabilidad de que el propietario venda todos los ejemplares en 10000 simulaciones:', severithing)
+    print('2. Numero promedio esperado de ejemplares no vendidos por dia en 10000 simulaciones:', nosellever)
+    print('3. Ingreso promedio neto esperado por dia con 10000 simulaciones: ', ganancias)
 
 SellAll()
-=======
-no_sell = 50 - np.array(range(35, 71))
-print("no sell ", no_sell)
-no_sell[np.where(no_sell < 0)[0]] = 0
-print("")
-print("Sold zeros", no_sell)
-print("")
-px = np.concatenate((np.ones(50-35)*1/45, np.ones(60-50)*1/30, np.ones(71-60)*1/33))
-print('px', px )
-print('px sum',np.sum(px))
-print('tam no sell', len(no_sell))
-print('tam px',len(px))
-
-# unsold estimated value
-np.sum(no_sell * px)
-print('no_sell x px', np.sum(no_sell * px))
-
-pos_all_sell = np.where(no_sell == 0)[0]
-print('probability of sell all',np.sum(px[pos_all_sell]))
-
-print ('np.sum(px[15:])',np.sum(px[15:]))
-
-no_sell_sim = np.random.choice(no_sell, p=px)
-
-Sold = 50 - no_sell_sim
-print('no vendidos simulacion',no_sell_sim)
-print('vendidos', Sold)
-
-#Simulation repetition prom
-#ganancia diaria
-ganancias = Sold*1 - 50*0.50
-print('ganancias', ganancias)
-
-def prob_sold():
-   
-    
-    return 
->>>>>>> b3f2f1ec2326417b6a9092b12edd6720076c9937
 
 
 
